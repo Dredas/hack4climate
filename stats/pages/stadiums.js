342267a2bdc62457563dd217f505abce809bceca
dredas <nerijus@ba329.com>
2020-02-29 22:52:19 +0200

    Pools

--- stats/pages/stadiums.js
+++ stats/pages/stadiums.js
@@ -22,14 +22,18 @@ export class MapContainer extends Component {
     }
 
     render() {
-        localStorage.setItem('liters', 50000000);
+        localStorage.setItem('co2', 11310);
 
-        const liters = localStorage.getItem('liters');
-        const pollLiters = 2500000;
+        const co2 = localStorage.getItem('co2');
 
-        const pools = Math.round(liters / pollLiters);
+        const oneMeter = 0.0000156;
+        const oneTreePerYear = 22;
+        const treesInStadium = 300;
 
-        const twentyFiveMeters = 0.00039;
+        const stadiums = Math.round(co2 / (treesInStadium * oneTreePerYear));
+
+        const l = 105 * oneMeter;
+        const h = 68 * oneMeter;
 
         const startLat = 54.69386;
         const startLng = 25.27532;
@@ -39,15 +43,15 @@ export class MapContainer extends Component {
         var currentA = startLat;
         var currentB = startLng;
 
-        for (let i = 0; i < pools; i++) {
+        for (let i = 0; i < stadiums; i++) {
 
-            currentB = currentB + 0.0005;
+            var currentB = currentB + 0.0023;
 
             const triangleCoords = [
                 {lat: currentA, lng: currentB},
-                {lat: currentA, lng: currentB + twentyFiveMeters},
-                {lat: currentA + (twentyFiveMeters * 2), lng: currentB + twentyFiveMeters},
-                {lat: currentA + (twentyFiveMeters * 2), lng: currentB},
+                {lat: currentA, lng: currentB + l},
+                {lat: currentA + h, lng: currentB + l},
+                {lat: currentA + h, lng: currentB},
             ];
 
             polygons.push(triangleCoords);
@@ -77,10 +81,10 @@ export class MapContainer extends Component {
                         return (
                             <Polygon
                                 paths={polygon}
-                                strokeColor="#0000FF"
+                                strokeColor="#008000"
                                 strokeOpacity={0.8}
                                 strokeWeight={2}
-                                fillColor="#0000FF"
+                                fillColor="#006400"
                                 fillOpacity={0.35}/>
                         )
                     })}
