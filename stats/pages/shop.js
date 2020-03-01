f7c6a9cd1e6361f18560f75a50adb617ae5aa2e6
ievaCode <iewute@yahoo.com>
2020-03-01 11:20:41 +0200

    minor

--- stats/pages/shop.js
+++ stats/pages/shop.js
@@ -37,9 +37,10 @@ const Shop = () => {
         e.preventDefault();
         const {name , value} = e.target;
         setProduct ( prevState => ({...prevState, [name]: value }));
-        Router.push({
-            pathname: '/shopping',
-        });
+        console.log(product);
+        // Router.push({
+        //     pathname: '/shopping',
+        // });
     };
 
     const handleChange = e => { 
@@ -64,28 +65,28 @@ const Shop = () => {
             <form className = 'shopping-form'onSubmit = {handleProductSubmit}>
             <p className = 'intro-radio'>Where will you do your shopping:</p>
                 <div className = 'radio-wrap'>
-                    <input type="radio" id="onePlace" name="origin" value="one place" checked/>
-                    <label for="onePlace">I will do all my shopping in one place</label>
+                    <input type="radio" id="onePlace" name="origin" value="one place"/>
+                    <label htmlFor="onePlace">I will do all my shopping in one place</label>
                 </div>
                 <div className = 'radio-wrap'>
                     <input className = 'radio-input' type="radio" id="different" name="typeOfShop" value="different places"/>
-                    <label for="different">I will shop around different places</label>
+                    <label htmlFor="different">I will shop around different places</label>
                 </div>
                 <p className = 'main-input-label'>What will you buy?</p>
                 <input className = 'main-input' type = 'text' placeholder = 'please type..' name = 'genName' value = {product.genName} onChange = {handleChange}/>                  
-                <label className = 'select-label'for="group">please specify: </label>
+                <label className = 'select-label'htmlFor="group">please specify: </label>
                 <select className = 'simple-select' id = 'group' >
                     {additional}
                 </select>                
                 <p className = 'intro-radio'>Is this product local or imported?</p>
                 <div className = 'radio-wrap'>
                     <div className = 'radio-input'>
-                        <input type="radio" id="originLocal" name="origin" value="local" checked/>
-                        <label for="originLocal">This product is local</label>
+                        <input type="radio" id="originLocal" name="origin" value="local"/>
+                        <label htmlFor="originLocal">This product is local</label>
                     </div>
                     <div className = 'radio-input'>
-                        <input type="radio" id="originImport" name="origin" value="imported" checked/>
-                        <label for="originLocal">This product is imported</label>
+                        <input type="radio" id="originImport" name="origin" value="imported"/>
+                        <label htmlFor="originLocal">This product is imported</label>
                     </div>
                 </div>
                 <button className = 'simple-submit' type= 'submit'>Next</button>
@@ -104,7 +105,7 @@ const Shop = () => {
                     background-color: #98FB98;
                     background:linear-gradient(rgba(0, 128, 0, 0.8),transparent);
                 }
-                form {
+                htmlForm {
                     width: 400px;
                     line-height: 2;
                     font-size: 20px;
